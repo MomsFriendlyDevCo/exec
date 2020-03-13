7c9713a5ed16b244386986db3165b6be03b3876c
Matt Carter <m@ttcarter.com>
2020-03-13 17:46:03 +1000

    TEST: More complete tests for STDIN types

--- test/stdin.js
+++ test/stdin.js
@@ -4,12 +4,23 @@ var mlog = require('mocha-logger');
 
 describe('STDIN piping', ()=> {
 
-	it('should support custom STDIN input', ()=>
+	it('should support buffer STDIN input', ()=>
 		exec('cat', {stdin: Buffer.from('Hello world'), buffer: true})
 			.then(output => expect(output).to.be.deep.equal('Hello world'))
 	);
 
-	it.skip('should handle STDIN streaming via a pipe', function() {
+	it('should support string STDIN input', ()=>
+		exec('tac', {stdin: 'A\nB\nC\n', buffer: true})
+			.then(output => expect(output).to.equal('C\nB\nA'))
+	);
+
+	it.skip('should handle STDIN streaming via a pipe (given the "inherit" shortcut)', function() {
+		// Skipped as this requires user interaction
+		this.timeout(60 * 1000);
+		return exec('bash', {stdin: 'inherit'})
+	});
+
+	it.skip('should handle STDIN streaming via a pipe (given a stream)', function() {
 		// Skipped as this requires user interaction
 		this.timeout(60 * 1000);
 		return exec('bash', {stdin: process.stdin})
